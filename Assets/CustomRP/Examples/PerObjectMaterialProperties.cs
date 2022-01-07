1b63a5e25bf441b5194d3214c20333b759d80b1f
meralium <polybreaker@gmail.com>
2022-01-07 21:12:32 +0100

    - material prop block (srp batcher doesn't work anymore)

---
+++ Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PerObjectMaterialProperties : MonoBehaviour
+{
+    private static int baseColorId = Shader.PropertyToID("_BaseColor");
+    private static MaterialPropertyBlock propertyBlock;
+
+    [SerializeField]
+    private Color baseColor = Color.white;
+
+    private void Awake()
+    {
+        OnValidate();
+    }
+
+    private void OnValidate()
+    {
+        if (propertyBlock == null)
+        {
+            propertyBlock = new MaterialPropertyBlock();
+        }
+
+        propertyBlock.SetColor(baseColorId, baseColor);
+        GetComponent<Renderer>().SetPropertyBlock(propertyBlock);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
