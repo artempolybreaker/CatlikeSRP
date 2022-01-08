61a8a171977e7fdc5914930d634d94dbf10e6db2
meralium <polybreaker@gmail.com>
2022-01-08 16:43:26 +0100

    - clipping

--- Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
+++ Assets/CustomRP/Examples/PerObjectMaterialProperties.cs
@@ -7,10 +7,15 @@ using Random = UnityEngine.Random;
 public class PerObjectMaterialProperties : MonoBehaviour
 {
     private static int baseColorId = Shader.PropertyToID("_BaseColor");
+    private static int cutoffId = Shader.PropertyToID("_Cutoff");
+
     private static MaterialPropertyBlock propertyBlock;
 
     [SerializeField]
     private Color baseColor = Color.white;
+
+    [SerializeField, Range(0f, 1f)] 
+    private float cutoff = 0.5f;
 
     private void Awake()
     {
@@ -25,7 +30,9 @@ public class PerObjectMaterialProperties : MonoBehaviour
         }
 
         // baseColor = new Color(Random.value, Random.value, Random.value);
+        // cutoff = Random.value;
         propertyBlock.SetColor(baseColorId, baseColor);
+        propertyBlock.SetFloat(cutoffId, cutoff);
         GetComponent<Renderer>().SetPropertyBlock(propertyBlock);
     }
 
--- Assets/CustomRP/Shaders/Unlit.shader
+++ Assets/CustomRP/Shaders/Unlit.shader
@@ -3,6 +3,8 @@ Shader "Custom RP/Unlit" {
     Properties {
         _BaseMap("Texture", 2D) = "white" {}
         _BaseColor("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+        _Cutoff("Alpha Cutoff", Range(0.0, 1.0)) = 0.5
+        [Toggle(_CLIPPING)] _Clipping("Alpha Clipping", Float) = 0
         [Enum(UnityEngine.Rendering.BlendMode)]_SrcBlend("Src blend", Float) = 1
         [Enum(UnityEngine.Rendering.BlendMode)]_DstBlend("Dst blend", Float) = 1
         [Enum(Off, 0, On, 1)]_ZWrite("Z write", Float) = 1
@@ -14,6 +16,7 @@ Shader "Custom RP/Unlit" {
             ZWrite [_ZWrite]
 
             HLSLPROGRAM
+            #pragma shader_feature _CLIPPING
             #pragma multi_compile_instancing
             #pragma vertex UnlitPassVertex
             #pragma fragment UnlitPassFragment
