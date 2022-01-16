b2e60e96083b076a59a431ba603334ea9336d7a9
meralium <polybreaker@gmail.com>
2022-01-16 19:01:26 +0100

    - basic lighting setup

---
+++ Assets/CustomRP/ShaderLibrary/Light.hlsl
@@ -0,0 +1,17 @@
+﻿#ifndef CUSTOM_LIGHT_INCLUDED
+#define CUSTOM_LIGHT_INCLUDED
+
+struct Light
+{
+    float3 color;
+    float3 direction;
+};
+
+Light GetDirectionalLight() {
+    Light light;
+    light.color = 1.0;
+    light.direction = float3(0,1,0);
+    return light;
+}
+
+#endif
