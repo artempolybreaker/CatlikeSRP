7ab3f97d76bd9c57caa540eac572e251cd0ee3c2
meralium <polybreaker@gmail.com>
2022-01-16 22:35:42 +0100

    - reflectivity

--- Assets/CustomRP/ShaderLibrary/BRDF.hlsl
+++ Assets/CustomRP/ShaderLibrary/BRDF.hlsl
@@ -1,6 +1,8 @@
 ﻿#ifndef CUSTOM_BRDF_INCLUDED
 #define CUSTOM_BRDF_INCLUDED
 
+#define MIN_REFLECTIVITY 0.04
+
 struct BRDF
 {
     float3 diffuse;
@@ -8,10 +10,18 @@ struct BRDF
     float roughness;
 };
 
+float OneMinusReflectivity(float metallic)
+{
+    float range = 1.0 - MIN_REFLECTIVITY;
+    return range - metallic * range;
+}
+
 BRDF GetBRDF(Surface surface)
 {
+    float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
+    
     BRDF brdf;
-    brdf.diffuse = surface.color;
+    brdf.diffuse = surface.color * oneMinusReflectivity;
     brdf.specular = 0.0;
     brdf.roughness = 1.0;
     return brdf;
