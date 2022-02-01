156c21712298c29310c4a14a5dc1ee9ce494e44a
meralium <polybreaker@gmail.com>
2022-02-01 21:45:56 +0100

    - preparing dir shadow matrices for the shader (For each fragment we have to sample the depth information from the appropriate tile in the shadow atlas. So we have to find the shadow texture coordinates for a give world-space position. We'll make this possible by creating a shadow transformation matrix for each shadowed directional light and sending them to the GPU)

--- Assets/CustomRP/Runtime/Shadows.cs
+++ Assets/CustomRP/Runtime/Shadows.cs
@@ -14,6 +14,9 @@ namespace CustomRP.Runtime
         private const uint MAX_SHADOWED_DIR_LIGHT_COUNT = 4;
 
         private static int dirShadowAtlasId = Shader.PropertyToID("_DirectionalShadowAtlas");
+        private static int dirShadowMatricesId = Shader.PropertyToID("_DirectionalShadowMatrices");
+
+        private static Matrix4x4[] dirShadowMatrices = new Matrix4x4[MAX_SHADOWED_DIR_LIGHT_COUNT];
 
         private CommandBuffer buffer = new CommandBuffer { name = BUFFER_NAME };
         private ShadowedDirLight[] shadowedDirLights = new ShadowedDirLight[MAX_SHADOWED_DIR_LIGHT_COUNT];
@@ -76,12 +79,13 @@ namespace CustomRP.Runtime
 
             int split = shadowedDirLightCount <= 1 ? 1 : 2;
             int tileSize = atlasSize / split;
-            
+
             for (int i = 0; i < shadowedDirLightCount; i++)
             {
                 RenderDirectionalShadows(i, split, tileSize);
             }
 
+            buffer.SetGlobalMatrixArray(dirShadowMatricesId, dirShadowMatrices);
             buffer.EndSample(BUFFER_NAME);
             ExecuteBuffer();
         }
@@ -93,18 +97,54 @@ namespace CustomRP.Runtime
             cullingResults.ComputeDirectionalShadowMatricesAndCullingPrimitives(light.visibleLightIndex, 0, 1,
                 Vector3.zero, tileSize, 0f, out var viewMatrix, out var projMatrix, out var splitData);
             shadowSettings.splitData = splitData;
-            SetTileViewport(index, split, tileSize);
+
+            // dirShadowMatrices[index] = projMatrix * viewMatrix; // -> conversion matrix from world space to light space
+            dirShadowMatrices[index] = ConvertToAtlasMatrix(
+                projMatrix * viewMatrix, SetTileViewport(index, split, tileSize), split
+            );
+
             buffer.SetViewProjectionMatrices(viewMatrix, projMatrix);
             ExecuteBuffer();
             context.DrawShadows(ref shadowSettings);
         }
 
-        private void SetTileViewport(int index, int split, float tileSize)
+        private Vector2 SetTileViewport(int index, int split, float tileSize)
         {
-            Vector2 offset = new Vector2(index % split, index/split);
+            Vector2 offset = new Vector2(index % split, index / split);
             buffer.SetViewport(new Rect(offset.x * tileSize, offset.y * tileSize, tileSize, tileSize));
+            return offset;
         }
-        
+
+        private Matrix4x4 ConvertToAtlasMatrix(Matrix4x4 m, Vector2 offset, int split)
+        {
+            if (SystemInfo.usesReversedZBuffer)
+            {
+                m.m20 = -m.m20;
+                m.m21 = -m.m21;
+                m.m22 = -m.m22;
+                m.m23 = -m.m23;
+            }
+
+            // clip space is defined inside a cube with with coordinates going from −1 to 1, with zero at its center.
+            // But textures coordinates and depth go from zero to one. We can bake this conversion into the matrix by
+            // scaling and offsetting the XYZ dimensions by half
+            float scale = 1f / split;
+            m.m00 = (0.5f * (m.m00 + m.m30) + offset.x * m.m30) * scale;
+            m.m01 = (0.5f * (m.m01 + m.m31) + offset.x * m.m31) * scale;
+            m.m02 = (0.5f * (m.m02 + m.m32) + offset.x * m.m32) * scale;
+            m.m03 = (0.5f * (m.m03 + m.m33) + offset.x * m.m33) * scale;
+            m.m10 = (0.5f * (m.m10 + m.m30) + offset.y * m.m30) * scale;
+            m.m11 = (0.5f * (m.m11 + m.m31) + offset.y * m.m31) * scale;
+            m.m12 = (0.5f * (m.m12 + m.m32) + offset.y * m.m32) * scale;
+            m.m13 = (0.5f * (m.m13 + m.m33) + offset.y * m.m33) * scale;
+            m.m20 = 0.5f * (m.m20 + m.m30);
+            m.m21 = 0.5f * (m.m21 + m.m31);
+            m.m22 = 0.5f * (m.m22 + m.m32);
+            m.m23 = 0.5f * (m.m23 + m.m33);
+
+            return m;
+        }
+
         public void Cleanup()
         {
             buffer.ReleaseTemporaryRT(dirShadowAtlasId);
